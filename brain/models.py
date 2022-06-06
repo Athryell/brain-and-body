38555ddef7eb1d90b25a57b207f0151d7633a29b
Athryell <aleprovvi@yahoo.it>
2022-06-06 15:54:08 +0200

    Changed img locations and updated models.py

--- brain/models.py
+++ brain/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 class Card(models.Model):
     name = models.CharField(max_length=64)
-    image = models.ImageField(upload_to='card-pics')
+    image_name = models.CharField(max_length=32, default='empty')
 
     def __str__(self):
         return self.name
+
+    def save(self):
+        self.image_name = f'{self.name.lower().replace(" ", "-")}.png'
+        return super(Card, self).save()
