ffb66f1d5479427418a883a0f6a878d66883f739
Athryell <aleprovvi@yahoo.it>
2022-06-06 15:21:10 +0200

    Changed directory name to store card pics

--- brain/models.py
+++ brain/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 class Card(models.Model):
     name = models.CharField(max_length=64)
-    image = models.ImageField(upload_to='card_pics')
-    # sibling = models.OneToOneField(Card2, on_delete=models.CASCADE, related_name='sibling')
+    image = models.ImageField(upload_to='card-pics')
 
     def __str__(self):
         return self.name
